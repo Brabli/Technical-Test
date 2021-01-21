3859a8157608a144286452a88a4afafd60963aa4
Brabli <67018167+Brabli@users.noreply.github.com>
2021-01-21 00:22:19 +0000

    First commit

---
+++ src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
@@ -0,0 +1,24 @@
+import { IColour } from '../people/interfaces/icolour';
+
+export class ColourNamesValueConverter {
+
+  toView(colours: IColour[]) {
+
+    // TODO: Step 4
+    //
+    // Implement the value converter function.
+    // Using the colours parameter, convert the list into a comma
+    // separated string of colour names. The names should be sorted
+    // alphabetically and there should not be a trailing comma.
+    //
+    // Example: 'Blue, Green, Red'
+
+      const colourNames: string = colours
+          .map(c => c.name)
+          .sort()
+          .join(", ");
+
+      return colourNames;
+  }
+
+}
---
+++ src/TechTest/Controllers/PeopleController.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TechTest.Repositories;
+using TechTest.Repositories.Models;
+
+
+namespace TechTest.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public PeopleController(IPersonRepository personRepository)
+        {
+            this.PersonRepository = personRepository;
+        }
+
+        private IPersonRepository PersonRepository { get; }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // TODO: Step 1
+            //
+            // Implement a JSON endpoint that returns the full list
+            // of people from the PeopleRepository. If there are zero
+            // people returned from PeopleRepository then an empty
+            // JSON array should be returned.
+
+            var people = PersonRepository.GetAll();
+            if (people.Count() == 0) {
+                return NotFound();
+            } else {
+                return Ok(people);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // TODO: Step 2
+            //
+            // Implement a JSON endpoint that returns a single person
+            // from the PeopleRepository based on the id parameter.
+            // If null is returned from the PeopleRepository with
+            // the supplied id then a NotFound should be returned.
+
+            Person person = PersonRepository.Get(id);
+
+            if (person == null) {
+                return NotFound();
+            } else {
+                return Ok(person);
+            }
+
+            
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, PersonUpdate personUpdate)
+        {
+            // TODO: Step 3
+            //
+            // Implement an endpoint that receives a JSON object to
+            // update a person using the PeopleRepository based on
+            // the id parameter. Once the person has been successfully
+            // updated, the person should be returned from the endpoint.
+            // If null is returned from the PeopleRepository then a
+            // NotFound should be returned.
+
+            Person person = PersonRepository.Get(id);
+
+            if (person == null) return NotFound();
+
+            person.Authorised = personUpdate.Authorised;
+            person.Enabled = personUpdate.Enabled;
+            person.Colours = personUpdate.Colours;
+
+            PersonRepository.Update(person);
+
+            return Ok(person);
+        }
+    }
+}
